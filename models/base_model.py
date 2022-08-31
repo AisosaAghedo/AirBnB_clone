a96482704319775a7a50290dabcfed655a4ad224
Aisosa <allysonaghedo@gmail.com>
2022-08-31 14:39:45 +0100

    entry point of the command intepreter

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
+""" Basemodel module """
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
     """
-    class BaseModel that defines all commom methods for other classes
+    class BaseModel that defines all common methods for other classes
     """
     def __init__(self, *args):
+        """ initialisation of the class BaseModel """
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def save(self):
+        """ updates the public instance attribute updated_at
+        with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """ returns a dictionary containing all keys/values of
+        __dict__ of the instance """
         dict1 = self.__dict__.copy()
         dict1['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         dict1['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
@@ -22,4 +29,5 @@ class BaseModel:
         return dict1
 
     def __str__(self):
+        """ prints  a string repr """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
