fb37910910d27b4bbce77b77e1b046dcc14ee0fa
Aisosa <allysonaghedo@gmail.com>
2022-09-02 12:03:55 +0100

    Added docstrings to the file storage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
+""" File storage module """
 import json
 from models.base_model import BaseModel
 
 
 class FileStorage:
+    """ class FileStorage """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """  returns the dictionary __objects """
         return FileStorage.__objects
 
     def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
+        """ serializes __objects to the JSON file """
         to_dict = {}
         for key, obj in FileStorage.__objects.items():
             to_dict[key] = obj.to_dict()
@@ -23,6 +28,7 @@ class FileStorage:
             json.dump(to_dict, file)
 
     def reload(self):
+        """ deserializes the JSON file to __objects """
         try:
             with open(FileStorage.__file_path, 'r') as f:
                 load = json.load(f)
